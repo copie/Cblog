45d766e4d2f6c0573abcfe56d168d81de65cf169
copie <admin@copie.cn>
2018-04-21 18:16:23 +0800

    添加 403 页面

--- app/main/errors.py
+++ app/main/errors.py
@@ -11,3 +11,8 @@ def page_not_founf(e):
 @main.app_errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'), 500
+
+
+@main.app_errorhandler(403)
+def page_not_founf(e):
+    return render_template('403.html'), 403
